bcf44723fe7341246203d599995b9d7f747376c0
andre <andre@budemji>
2024-05-08 09:10:30 +0200

    Remove top cancel button from intro and game over.

--- main.js
+++ main.js
@@ -218,12 +218,14 @@ class Intro extends Phaser.Scene
         );
 
         // Add back button.
+        /*
         let back = this.add.image(width - 40, 40, "back");
         back.setInteractive();
         back.on("pointerdown", function (p) {
             this.data["minigame"].scene.stop();
             this.scene.start("menu");
         }, this);
+        */
 
         // Cancel button
         let size = 100;
@@ -292,6 +294,7 @@ class GameOver extends Phaser.Scene
         );
 
         // Add back button.
+        /*
         let back = this.add.image(width - 40, 40, "back");
         back.setInteractive();
         back.on("pointerdown", function (p) {
@@ -299,6 +302,7 @@ class GameOver extends Phaser.Scene
             this.scene.stop();
             this.scene.start("menu");
         }, this);
+        */
 
         // "Cancel button"
         let size = 100;
